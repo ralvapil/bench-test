f245cc17a1f26024d79f0b9a7d6b40f6abae4a18
Ram Alvapillai <ramanan.a125@gmail.com>
2021-06-25 16:05:54 -0700

    Implemented project folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import React from 'react'
+import { MainView } from './views'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      App
+    <div>
+      <MainView/>
     </div>
-  );
+  )
 }
-
-export default App;
