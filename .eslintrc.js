4b27914f0c2c11b6941922b096d1963b2abe38f8
Ram Alvapillai <ramanan.a125@gmail.com>
2021-06-26 13:33:14 -0700

    Updated eslint config to allow nested ternary operators

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,25 +3,26 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    "react/react-in-jsx-scope": 0,
-    "react/prop-types": 0,
-    "import/no-unresolved": 0,
-    "import/prefer-default-export": 0,
-    "no-plusplus": [2, { allowForLoopAfterthoughts: true }],
-    "react/jsx-filename-extension": [
+    'react/react-in-jsx-scope': 0,
+    'react/prop-types': 0,
+    'import/no-unresolved': 0,
+    'import/prefer-default-export': 0,
+    'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+    'no-nested-ternary': 0,
+    'react/jsx-filename-extension': [
       1,
       {
-        extensions: [".js", ".jsx"],
+        extensions: ['.js', '.jsx'],
       },
     ],
   },
